d71c30a12d4c8a1179015db15f52058294521088
Cameron Cruz <cameroncruz@DN0a22f22d.SUNet>
2018-07-11 15:55:54 -0700

    Add comments to DogBot class

--- DogBot.py
+++ DogBot.py
@@ -12,10 +12,24 @@ class DogBot:
         self.loadSounds()
 
     def loadSounds(self):
+        """Load sound effect files.
+
+        Initializes the pygame mixer and loads sound files from _SOUND_DIR
+        into mixer Sound objects that can be accessed from the self.sounds
+        dictionary. This prevents redundant loading of sound files.
+        """
         mixer.init()
         for entry in os.scandir(_SOUND_DIR):
             if entry.name.endswith(".wav"):
                 self.sounds[entry.path] = mixer.Sound(entry.path)
 
     def playSound(self, soundPath):
+        """Play a sound effect.
+
+        Plays the sound effect that we have preloaded from the specified
+        path.
+
+        Arguments:
+            soundPath {[type]} -- path of sound effect to play
+        """
         self.sounds[soundPath].play()
