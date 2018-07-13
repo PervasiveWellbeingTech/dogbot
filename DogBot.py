ea531a2289f5fa8e650087bd2f8151fc95ec5471
Cameron Cruz <cameroncruz@DN0a22f632.SUNet>
2018-07-13 11:46:28 -0700

    Implement emotion recognition with deepaffects

--- DogBot.py
+++ DogBot.py
@@ -1,15 +1,21 @@
 import os
+import base64
 from pygame import mixer
+import deepaffects as da
+from deepaffects.rest import ApiException
 
+da.configuration.api_key['apikey'] = 'NL47UgAe2MvSk9N7O6CE4fqYM0XiNa4i'
 _SOUND_DIR = "sfx"
 
 
 class DogBot:
     name = ""
     sounds = {}
+    er = None
 
     def __init__(self):
         self.loadSounds()
+        self.initEmotionRecognizer()
 
     def loadSounds(self):
         """Load sound effect files.
@@ -23,6 +29,9 @@ class DogBot:
             if entry.name.endswith(".wav"):
                 self.sounds[entry.path] = mixer.Sound(entry.path)
 
+    def initEmotionRecognizer(self):
+        self.er = da.EmotionApi()
+
     def playSound(self, soundPath):
         """Play a sound effect.
 
@@ -30,6 +39,39 @@ class DogBot:
         path.
 
         Arguments:
-            soundPath {[type]} -- path of sound effect to play
+            soundPath {string} -- path of sound effect to play
         """
         self.sounds[soundPath].play()
+
+    def recognizeEmotion(self, speech_audio):
+        """Recognize the emotion for user speech input.
+
+        Takes in audio of a user speaking and returns a prediction for
+        the emotion and its associated probability.
+
+        Currently using the DeepAffects API for the emotion recognition
+        step. As a result, the audio must be <2min and match the
+        metadata variables declared below.
+
+        Arguments:
+            speech_audio {bytes} -- Audio of user talking.
+
+        Returns:
+            dict -- Contains the emotion and its probability score.
+        """
+        encoding = 'Wave'
+        sample_rate = 44100
+        language_code = 'en-US'
+        content = base64.b64encode(speech_audio).decode('utf-8')
+
+        body = da.Audio(encoding=encoding, sample_rate=sample_rate,
+                        language_code=language_code, content=content)
+
+        try:
+            response = self.er.sync_recognise_emotion(body)
+            prediction = max(response, key=lambda x: x.to_dict()['score'])
+            print("Prediction: {}".format(prediction.to_str()))
+            return prediction.to_dict()
+        except ApiException as e:
+            print("Exception when calling EmotionApi->sync_recognise_emotion: {}\n".format(e))
+            return None
--- main.py
+++ main.py
@@ -8,13 +8,14 @@ def main():
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
         while True:
             print("Say something!")
             audio = r.listen(source)
             try:
                 print("You said:- " + r.recognize_google(audio))
                 dogbot.playSound("sfx/whine1.wav")
-                #print(type(audio.get_wav_data()))
+                dogbot.recognizeEmotion(audio.get_wav_data())
             except sr.UnknownValueError:
                 print("Could not understand audio")
 
